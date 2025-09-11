c3d3bff1aeb0b9ef591dc03bd313707b01648b5d
nathalitzoy-A <nathali.tzoy@guate5.com>
2025-09-11 16:36:14 -0600

    feat: :art: servicio Giphy e interfaces para integración de la API

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from "@angular/common/http";
+import { inject, Injectable } from "@angular/core";
+import { environment } from "@environments/environment";
+import { GiphyResponse } from "../interface/gipy.interfaces";
+
+@Injectable({providedIn:'root'})
+export class GifService{
+   
+    private http = inject (HttpClient)
+
+    constructor(){
+        this.loadTrendingGifs()
+    }
+    
+    loadTrendingGifs(){
+        this.http.get<GiphyResponse>(`${environment.giphyUrl}/gifs/trending`,{
+            params:{
+                api_key: environment.giphyApiKey,
+                limit: 20,
+            }
+        })
+    }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  companyName: 'Gifs',
+  companyName2: 'App',
+  companySlogan: 'Maneja tus gifs',
+
+  giphyApiKey: '0Qm3Z44QruADvsTAH6Mz9nSPk0TOBzQF',
+  giphyUrl: 'https://api.giphy.com/v1',
+  //URLS
+};
