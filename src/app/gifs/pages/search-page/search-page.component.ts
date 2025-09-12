51d2e4655fccf98d24eb929ef8ddea12e593208f
nathalitzoy-A <nathali.tzoy@guate5.com>
2025-09-12 11:51:45 -0600

    feat: :sparkles: implementar funcionalidad de busqueda y mejorar el diseño de la página de busqueda

--- src/app/gifs/pages/search-page/search-page.component.ts
+++ src/app/gifs/pages/search-page/search-page.component.ts
@@ -1,9 +1,20 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {  Component, inject, signal } from '@angular/core';
+import { GifListComponent } from '../../components/gif-list/gif-list.component';
+import { GifService } from '../../services/gifs.service';
+import { Gif } from '../../interface/gif.interface';
 
 @Component({
   selector: 'app-search-page',
-  imports: [],
+  imports: [GifListComponent],
   templateUrl: './search-page.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class SearchPageComponent {}
+export default class SearchPageComponent {
+  GifService = inject(GifService)  
+  gifs = signal<Gif[]>([]);
+
+  onSearch(query: string) {
+    this.GifService.searchGifs(query).subscribe((resp) => {
+      this.gifs.set(resp);
+    });
+  };
+}
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,34 +1,54 @@
-import { HttpClient } from "@angular/common/http";
-import { inject, Injectable, signal } from "@angular/core";
-import { environment } from "@environments/environment";
-import { GiphyResponse } from "../interface/gipy.interfaces";
-import { Gif } from "../interface/gif.interface";
-import { GifMapper } from "../mapper/gif.mapper";
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { environment } from '@environments/environment';
+import { GiphyResponse } from '../interface/gipy.interfaces';
+import { Gif } from '../interface/gif.interface';
+import { GifMapper } from '../mapper/gif.mapper';
+import { map } from 'rxjs';
 
-@Injectable({providedIn:'root'})
-export class GifService{
-   
-    private http = inject (HttpClient);
+@Injectable({ providedIn: 'root' })
+export class GifService {
+  private http = inject(HttpClient);
 
-    trendingGifs = signal<Gif[]>([])
+  trendingGifs = signal<Gif[]>([]);
 
-    trendinfGifsLoading = signal(true);
+  trendinfGifsLoading = signal(true);
 
-    constructor(){
-        this.loadTrendingGifs()
-    }
-    
-    loadTrendingGifs(){
-        this.http.get<GiphyResponse>(`${environment.giphyUrl}/gifs/trending`,{
-            params:{
-                api_key: environment.giphyApiKey,
-                limit: 20,
-            }
-        }).subscribe( (resp) =>{
-            const gifs = GifMapper.mapGiphyItemToGifArray(resp.data);
-            this.trendingGifs.set(gifs);
-            this.trendinfGifsLoading.set(false);
-            console.log({gifs});
-        });
-    }
+  constructor() {
+    this.loadTrendingGifs();
+  }
+
+  loadTrendingGifs() {
+    this.http
+      .get<GiphyResponse>(`${environment.giphyUrl}/gifs/trending`, {
+        params: {
+          api_key: environment.giphyApiKey,
+          limit: 20,
+        },
+      })
+      .subscribe((resp) => {
+        const gifs = GifMapper.mapGiphyItemToGifArray(resp.data);
+        this.trendingGifs.set(gifs);
+        this.trendinfGifsLoading.set(false);
+        console.log({ gifs });
+      });
+  }
+
+  searchGifs(query: string) {
+    return this.http
+      .get<GiphyResponse>(`${environment.giphyUrl}/gifs/search`, {
+        params: {
+          api_key: environment.giphyApiKey,
+          limit: 20,
+          q:query,
+        },
+      }).pipe(
+        map(({data}) => data),
+        map((items) => GifMapper.mapGiphyItemToGifArray(items)),
+      );
+      // .subscribe((resp) => {
+      //   const gifs = GifMapper.mapGiphyItemToGifArray(resp.data);
+      //   console.log({search: gifs});
+      // });
+  }
 }
